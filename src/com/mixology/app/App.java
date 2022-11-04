df8b51625487e8ba885d943c76fff0c4c5f6d864
StephanieCobble <cobble.stephanie@gmail.com>
2022-11-04 12:27:49 -0700

    Beginning of the controller

---
+++ src/com/mixology/app/App.java
@@ -0,0 +1,59 @@
+package com.mixology.app;
+
+import com.apps.util.Prompter;
+import com.mixology.RegisteredUser;
+import com.mixology.UnregisteredUser;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    private final Scanner scanner = new Scanner(System.in);
+    private final Prompter prompter = new Prompter(new Scanner(System.in));
+
+
+    public void execute() {
+        welcome();
+        String user = userSelection(); // Register, or cont as guest
+        search();
+        showResults();
+        save(); // would you like to save
+        restart(); // prompt User to search again
+        goodbye();
+    }
+
+    private void welcome() {
+        System.out.println("Welcome to Master Mixologist!");
+    }
+
+    private String userSelection() {
+
+        String user = null;
+
+        String input = prompter.prompt(
+                "Would you like to register? Y/N ",
+                "\\s*" + "(y|Y|n|N)" + "\\s*", ""
+        ).trim().toUpperCase();
+
+//        user = ("Y".equals(input)) ? RegisteredUser : UnregisteredUser;
+
+        return user;
+    }
+
+    private void search() {
+    }
+
+    private void showResults() {
+    }
+
+    private void save() {
+    }
+
+    private void restart() {
+    }
+
+    private void goodbye() {
+    }
+
+
+}
