76bfea2654ff321d02c203ba58239a247129c9e5
Uyen Nguyen <nguyeud@gmail.com>
2022-11-04 19:46:57 -0400

    Added Recipe class to create a new Recipe class when an ID request is made (when user chooses to view a recipe

---
+++ src/com/mixology/database/IdRequest.java
@@ -0,0 +1,76 @@
+package com.mixology.database;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+class IdRequest {
+    // Fields
+    private static final String urlString = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+    // Methods
+    /*
+     * Method to fetch data from the server
+     */
+    public static Recipe sendRequest(int id) {
+        Recipe recipe = null;
+
+        try {
+            URL url = new URL(urlString + id);
+
+            // Open GET request to URL
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.setRequestMethod("GET");
+            request.connect();
+
+            // Getting the response code
+            int responseCode = request.getResponseCode();
+
+            // If response code != 200, then not successful, throw exception
+            if (responseCode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responseCode);
+            } else {
+                InputStreamReader inputStreamReader = new InputStreamReader((InputStream) request.getContent());
+
+                // Map to JSON objects
+                JSONObject dataObj = (JSONObject) new JSONParser().parse(inputStreamReader);
+
+                // Get the required object from the above create object
+                // In the API, "drinks" is the required object (this is the same among all the filters)
+                JSONArray dataArr = (JSONArray) dataObj.get("drinks");
+                JSONObject newObj = (JSONObject) dataArr.get(0);
+
+                // Create new Recipe object
+                String rId = (String) newObj.get("idDrink");
+                String rName = (String) newObj.get("strDrink");
+                String rCategory = (String) newObj.get("strCategory");
+                String rInstructions = (String) newObj.get("strInstructions");
+                boolean rIsAlcoholic = ((String) newObj.get("strAlcoholic")).equals("Alcoholic");
+                String rImage = (String) newObj.get("strDrinkThumb");
+
+                // Iterate over ingredient to add to a list for Recipe class
+                List<String> rIngredients = new ArrayList<>();
+                for (int i = 1; i <= 15; i++) {
+                    String ingredient = (String) newObj.get("strIngredient" + i);
+                    String amount = (String) newObj.get("strMeasure" + i);
+
+                    if (ingredient != null) {
+                        rIngredients.add(amount + " - " + ingredient);
+                    }
+                }
+
+                recipe = new Recipe(rId, rName, rCategory, rIngredients, rInstructions, rIsAlcoholic, rImage);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return recipe;
+    }
+}
---
+++ src/com/mixology/database/Recipe.java
@@ -0,0 +1,93 @@
+package com.mixology.database;
+
+import java.util.List;
+
+class Recipe {
+    private String id;
+    private String name;
+    private String category;
+    private List<String> ingredientList;
+    private String instructions;
+    private boolean isAlcoholic;
+    private String image;
+
+    public Recipe(String id, String name, String category, List<String> ingredientList, String instructions,
+                  boolean isAlcoholic, String image) {
+        setId(id);
+        setName(name);
+        setCategory(category);
+        setIngredientList(ingredientList);
+        setInstructions(instructions);
+        setAlcoholic(isAlcoholic);
+        setImage(image);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public List<String> getIngredientList() {
+        return ingredientList;
+    }
+
+    public void setIngredientList(List<String> ingredientList) {
+        this.ingredientList = ingredientList;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public boolean isAlcoholic() {
+        return isAlcoholic;
+    }
+
+    public void setAlcoholic(boolean alcoholic) {
+        isAlcoholic = alcoholic;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "id='" + getId() + '\'' +
+                ", name='" + getName() + '\'' +
+                ", category='" + getCategory() + '\'' +
+                ", ingredientList=" + getIngredientList() +
+                ", instructions='" + getInstructions() + '\'' +
+                ", isAlcoholic=" + isAlcoholic() +
+                ", image='" + getImage() + '\'' +
+                '}';
+    }
+}
