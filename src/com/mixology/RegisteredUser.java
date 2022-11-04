7d5af13337be3102a84cfe6ce646d44a38092616
StephanieCobble <cobble.stephanie@gmail.com>
2022-11-04 09:45:37 -0700

    Basic User, RegisteredUser, & UnregisteredUser classes laid out.

---
+++ src/com/mixology/RegisteredUser.java
@@ -0,0 +1,64 @@
+package com.mixology;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class RegisteredUser implements User {
+    private String name;
+    private String tagline;
+
+    // pseudo
+//    private List<Database> drinks = new ArrayList<>();
+
+    public RegisteredUser(String name, String tagline) {
+        setName(name);
+        setTagline(tagline);
+    }
+
+    @Override
+    public void search() {
+
+    }
+
+    public void save() {
+
+    }
+
+    //pseudo
+//    public void save(Database drink) {
+//        drinks.add(drink);
+//    }
+
+    public void delete() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTagline() {
+        return tagline;
+    }
+
+    public void setTagline(String tagline) {
+        this.tagline = tagline;
+    }
+
+    //pseudo
+//    public List<Database> getSaved() {
+//        return Collections.unmodifiableList(drinks);
+//    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: name = %s, tagline = %s\n",
+                getClass().getSimpleName(), getName(), getTagline());
+    }
+}
---
+++ src/com/mixology/UnregisteredUser.java
@@ -0,0 +1,9 @@
+package com.mixology;
+
+public class UnregisteredUser implements User {
+
+    @Override
+    public void search() {
+
+    }
+}
